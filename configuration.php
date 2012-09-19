ae52363ed49bc1957a41947e1bcc8d4533f5356c
arwenict <caeschli@arwen.com.au>
2012-09-20 07:52:20 +1000

    Update configuration.php
    Just a small change 

--- configuration.php
+++ configuration.php
@@ -1,7 +1,7 @@
 <?php
 class JConfig {
 	public $offline = '0';
-	public $offline_message = 'This site is down for maintenance.<br /> Please check back again soon.';
+	public $offline_message = 'This site is down for maintenance.<br /> Please check back again Soooon.';
 	public $display_offline_message = '1';
 	public $offline_image = '';
 	public $sitename = 'prjR';
