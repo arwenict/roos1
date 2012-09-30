b4b0ab2b40a238f99ef2819e41938bf803568901
ramsud <ramsud@gmail.com>
2012-09-30 19:17:36 +1000

    Update kendoui/reports/classes.php

--- kendoui/reports/classes.php
+++ kendoui/reports/classes.php
@@ -72,7 +72,7 @@
 								StartDate: { type: "date", editable: false },
                                                                 StartTime: { type: "text", editable: false },
 								Location: { editable: false },
-								InstructorName: { editable: false },
+								InstructorName: { type: "text", editable: true },
 								Minutes: { editable: false},
 								HourlyRate: { type: "number", validation: { required: true, min: 0} },
 								AttendeeNumber: { type: "number", validation: { required: true, min: 0} },
