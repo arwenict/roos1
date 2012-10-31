b0b85c145383642fcb232cf3b11d29b77c8a18a9
Maxim Pak <zucler@gmail.com>
2012-10-31 11:35:42 +1100

    dev

--- components/com_wrapper/views/wrapper/tmpl/default.php
+++ components/com_wrapper/views/wrapper/tmpl/default.php
@@ -21,23 +21,7 @@ function iFrameHeight() {
 	}
 }
 </script>
-<div id="overlay" class="popup-wrapper" >
-    <div class="popup resizable">
-        <div id='cpt_lghtbx' class="mid fixedBox" style='overflow-y:scroll;'>
-            <a href="#" id="close-captions" onclick="javascript:closePopUp()" class="close">&nbsp;</a>
-            <h2>Help</h2>
 
-            <div class="hightlight-box">
-                <h3>Sub heading 1</h3>
-                <?php if ($this->params->page_title == "Timetable") {?>
-                <p>Timetable help</p>
-                <?php } ?>
-            </div>
-
-
-        </div>
-    </div>
-</div>
 <div class="contentpane<?php echo $this->pageclass_sfx; ?>">
 <?php if ($this->params->get('show_page_heading', 1)) : ?>
 	<h1>
