fc811c0c55fc706095897bb9aebad8a19bc1ed45
Maxim Pak <mpak@Maxim-Paks-iMac.local>
2012-11-03 17:21:08 +1100

    dev

--- kendoui/reports/instructors.php
+++ kendoui/reports/instructors.php
@@ -105,7 +105,7 @@
 				{ field: "email", title: "Email", width: 180, filterable: false },
 				{ field: "skills", title: "Skills", width: 140, filterable: false },     
 				{ field: "permcov", title: "Perm / Cover", width: 70, filterable: true },
-				{ field: "locations", title: "Locations", width: 100, values:location_array} 
+				{ field: "locations", title: "Locations", width: 100} 
                                 ],
 				toolbar: [ 
 				
