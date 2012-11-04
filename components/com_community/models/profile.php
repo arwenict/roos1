c27df12bbd9066b771798217d77944430f174613
Maxim Pak <mpak@Maxim-Paks-iMac.local>
2012-11-04 20:30:57 +1100

    Revert "Revert "dev""
    This reverts commit 06c8817ff268efc4367268074bb4c2f55052029f.

--- components/com_community/models/profile.php
+++ components/com_community/models/profile.php
@@ -82,7 +82,6 @@ class CommunityModelProfile extends JCCModel
 	
 	public function _loadAllFields($filter = array() , $type = COMMUNITY_DEFAULT_PROFILE )
 	{
-            echo "here";exit;die();
 		if($this->_allField == null)
 		{
 			$this->_allField = array();
@@ -119,7 +118,7 @@ class CommunityModelProfile extends JCCModel
 			}
 			
 			$query .= ' ORDER BY '.$db->nameQuote('ordering');
-                       
+
 			$db->setQuery($query);
 
 			
