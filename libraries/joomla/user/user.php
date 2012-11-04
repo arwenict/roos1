ebfe45bcdc41ad6910d517e8eb576e20f2c043aa
Maxim Pak <mpak@Maxim-Paks-iMac.local>
2012-11-04 20:24:38 +1100

    Revert "dev"
    This reverts commit ebc7dece81f74a9d47992031565037d0f8622d3a.

--- libraries/joomla/user/user.php
+++ libraries/joomla/user/user.php
@@ -532,6 +532,7 @@ class JUser extends JObject
 			{
 				$file = $parampath . '/' . 'user.xml';
 			}
+                        echo "file: $file"; exit;
 			$this->_params->loadSetupFile($file);
 		}
 
