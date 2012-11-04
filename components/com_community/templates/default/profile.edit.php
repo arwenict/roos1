ae11880e73680f5afd65180002e5a6eeffe315a9
Maxim Pak <mpak@Maxim-Paks-iMac.local>
2012-11-04 20:26:06 +1100

    Revert "dev"
    This reverts commit 02755d19137ae9e8909f850df8d544a2b5f3d63e.

--- components/com_community/templates/default/profile.edit.php
+++ components/com_community/templates/default/profile.edit.php
@@ -100,7 +100,7 @@ foreach ( $fields as $name => $fieldGroup )
                                                         <label id=\"lblfield{$f->id}\" for=\"field{$f->id}\" class='label'>$fieldRequired {$f->name}</label>
                                                     </td>
                                                     <td class='value'>
-                                                        <div class='jomNameTips tipRight' original-title='Which club locations are preferred by instructor'>
+                                                        <div class='jomNameTips' original-title='Which club locations are preferred by instructor'>
                                                             $options
                                                             <input type='hidden' id='field22' name='field22' />
                                                         </div>
